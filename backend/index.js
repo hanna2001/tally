73d59a7e058c5f70dff4a07dfe62aa9e946ffa28
hanna2001 <hanblessab@gmail.com>
2026-05-29 21:29:31 +0530

    added data compression

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const cors = require("cors");
+const compression = require("compression");
 const transactionRoutes = require("./routes/transactions");
 const sheetsRouter = require("./routes/sheets")
  
 const app = express();
+app.use(compression());
 const PORT = process.env.PORT || 4000;
  
 app.use(cors({ origin: "http://localhost:5173" }));
